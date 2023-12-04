7ef3539e7866610661f93179d9edbc4870d106e9
AmberComet <117096797+AmberComet@users.noreply.github.com>
2023-12-04 10:43:18 -0600

    built out Query class

--- src/main/java/edu/washburn/Query.java
+++ src/main/java/edu/washburn/Query.java
@@ -1,8 +1,18 @@
 package edu.washburn;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Query {
     private String url;
     private String html;
+    private String currentPrice;
+    private String itemName;
 
     public Query(){
         url ="";
@@ -10,8 +20,64 @@ public class Query {
 
     public Query(String url){
         this.url = url;
+    
+        try {
+            scrapeSite();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    
+    private void scrapeSite() throws IOException{
+        //opening a connection to the website
+        URL obj = new URL(url); //could prob come up with a better name for this object
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //headers for the connection so it works
+        con.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120." );
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Response code: " + responseCode);
+
+        //reads the website from buffer and builds the HTML
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        this.html = response.toString();
+        
+    }
+
+    private void captureData(){
+        String regex = "<span class=\"a-offscreen\">(.*?)</span>";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(html);
+
+        String capturedData = null;
+        if (matcher.find()) {
+            capturedData = matcher.group(1);
+        }
+        System.out.println("CapturedData: "+capturedData);
+        this.currentPrice=capturedData;
+    }
+
+    public String getPrice(){
+        return currentPrice;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+
+
+    public void dumpHTML(){
+        System.out.println(html);
+    }
 
 }
