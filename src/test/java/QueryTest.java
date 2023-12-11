1c3c2d671158a54f26f84e2db8e11f616bc2ecad
AmberComet <117096797+AmberComet@users.noreply.github.com>
2023-12-10 20:47:36 -0600

    fixed tests

--- src/test/java/QueryTest.java
+++ src/test/java/QueryTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.washburn.Item;
 import edu.washburn.Query;
 
 public class QueryTest {
@@ -30,6 +31,18 @@ public class QueryTest {
         query.captureDataFromFile();
         assertNull(query.getPrice());
     }
-
-
+    
+    
+    @Test
+    public void testgetUrl() {
+        Item i = new Item("www.google7.com", true, false, 35.0);
+        String result = i.getUrl();
+        assertEquals("www.google7.com", result);
+    }
+    @Test
+    public void testgetUrl1() {
+        Item i = new Item("www.amazon.com", true, false, 35.0);
+        String result = i.getUrl();
+        assertEquals("www.google7.com", result);
+    }
 }
